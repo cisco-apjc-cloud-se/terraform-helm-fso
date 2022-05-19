5be10b55bd5e0367b6ca5784da5507a1987eb90a
Richard <richwats@cisco.com>
2022-05-19 18:07:22 +1000

    test

--- main.tf
+++ main.tf
@@ -74,43 +74,43 @@ module "appd" {
   install_metrics_server  = var.appd.install_metrics_server
 
   ### InfraViz ###
-  infraviz_enable_container_hostid  = var.infraviz.enable_container_hostid
-  infraviz_enable_dockerviz         = var.infraviz.enable_dockerviz
-  infraviz_enable_serverviz         = var.infraviz.enable_serverviz
-  infraviz_enable_masters           = var.infraviz.enable_masters
-  infraviz_stdout_logging           = var.infraviz.stdout_logging
+  infraviz_enable_container_hostid  = var.appd.infraviz.enable_container_hostid
+  infraviz_enable_dockerviz         = var.appd.infraviz.enable_dockerviz
+  infraviz_enable_serverviz         = var.appd.infraviz.enable_serverviz
+  infraviz_enable_masters           = var.appd.infraviz.enable_masters
+  infraviz_stdout_logging           = var.appd.infraviz.stdout_logging
 
   ### NetViz ###
-  netviz_enabled = try(var.netviz.enabled, null) #var.netiz != null ? var.netviz.enabled : null
+  netviz_enabled = try(var.appd.netviz.enabled, null) #var.netiz != null ? var.netviz.enabled : null
 
   ### Cluster Agent ###
-  clusteragent_montior_namespace_regex = try(var.cluster.montior_namespace_regex, null) #var.cluster != null ? var.cluster.montior_namespace_regex : null
+  clusteragent_montior_namespace_regex = try(var.appd.cluster.montior_namespace_regex, null) #var.cluster != null ? var.cluster.montior_namespace_regex : null
 
   ### Auto Instrumentation ###
-  autoinstrumentation_enabled                     = try(var.autoinstrument.enabled, false)# var.autoinstrument != null ? var.autoinstrument.enabled : false
-  autoinstrumentation_namespace_regex             = try(var.autoinstrument.namespace_regex, null)# var.autoinstrument.namespace_regex != null ? var.autoinstrument.namespace_regex : null
-  autoinstrumentation_default_appname             = try(var.autoinstrument.default_appname, null)#var.autoinstrument.default_appname != null ? var.autoinstrument.default_appname : null
-  autoinstrumentation_appname_strategy            = try(var.autoinstrument.appname_strategy, null)#var.autoinstrument.appname_strategy != null ? var.autoinstrument.appname_strategy : null
-  autoinstrumentation_java_runasuser              = try(var.autoinstrument.java.runasuser, null)#var.autoinstrument.java.runasuser != null ? var.autoinstrument.java.runasuser : null
-  autoinstrumentation_java_image                  = try(var.autoinstrument.java.image, null)#var.autoinstrument.java.image != null ? var.autoinstrument.java.image : null
-  autoinstrumentation_java_imagepullpolicy        = try(var.autoinstrument.java.imagepullpolicy, null)#var.autoinstrument.java.imagepullpolicy != null ? var.autoinstrument.java.imagepullpolicy : null
-  autoinstrumentation_dotnetcore_runasuser        = try(var.autoinstrument.dotnetcore.runasuser, null)#var.autoinstrument.dotnetcore.runasuser != null ? var.autoinstrument.dotnetcore.runasuser : null
-  autoinstrumentation_dotnetcore_image            = try(var.autoinstrument.dotnetcore.image, null)#var.autoinstrument.dotnetcore.image != null ? var.autoinstrument.dotnetcore.image : null
-  autoinstrumentation_dotnetcore_imagepullpolicy  = try(var.autoinstrument.dotnetcore.imagepullpolicy, null)#var.autoinstrument.dotnetcore.imagepullpolicy != null ? var.autoinstrument.dotnetcore.imagepullpolicy : null
-  autoinstrumentation_nodejs_runasuser            = try(var.autoinstrument.nodejs.runasuser, null)#var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
-  autoinstrumentation_nodejs_image                = try(var.autoinstrument.nodejs.image, null)#var.autoinstrument.nodejs.image != null ? var.autoinstrument.nodejs.image : null  # Note: No Latest Image Tag
-  autoinstrumentation_nodejs_imagepullpolicy      = try(var.autoinstrument.nodejs.runasuser,null)#var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
+  autoinstrumentation_enabled                     = try(var.appd.autoinstrument.enabled, false)# var.autoinstrument != null ? var.autoinstrument.enabled : false
+  autoinstrumentation_namespace_regex             = try(var.appd.autoinstrument.namespace_regex, null)# var.autoinstrument.namespace_regex != null ? var.autoinstrument.namespace_regex : null
+  autoinstrumentation_default_appname             = try(var.appd.autoinstrument.default_appname, null)#var.autoinstrument.default_appname != null ? var.autoinstrument.default_appname : null
+  autoinstrumentation_appname_strategy            = try(var.appd.autoinstrument.appname_strategy, null)#var.autoinstrument.appname_strategy != null ? var.autoinstrument.appname_strategy : null
+  autoinstrumentation_java_runasuser              = try(var.appd.autoinstrument.java.runasuser, null)#var.autoinstrument.java.runasuser != null ? var.autoinstrument.java.runasuser : null
+  autoinstrumentation_java_image                  = var.appd.autoinstrument.java.image #try(var.appd.autoinstrument.java.image, null)#var.autoinstrument.java.image != null ? var.autoinstrument.java.image : null
+  autoinstrumentation_java_imagepullpolicy        = try(var.appd.autoinstrument.java.imagepullpolicy, null)#var.autoinstrument.java.imagepullpolicy != null ? var.autoinstrument.java.imagepullpolicy : null
+  autoinstrumentation_dotnetcore_runasuser        = try(var.appd.autoinstrument.dotnetcore.runasuser, null)#var.autoinstrument.dotnetcore.runasuser != null ? var.autoinstrument.dotnetcore.runasuser : null
+  autoinstrumentation_dotnetcore_image            = try(var.appd.autoinstrument.dotnetcore.image, null)#var.autoinstrument.dotnetcore.image != null ? var.autoinstrument.dotnetcore.image : null
+  autoinstrumentation_dotnetcore_imagepullpolicy  = try(var.appd.autoinstrument.dotnetcore.imagepullpolicy, null)#var.autoinstrument.dotnetcore.imagepullpolicy != null ? var.autoinstrument.dotnetcore.imagepullpolicy : null
+  autoinstrumentation_nodejs_runasuser            = try(var.appd.autoinstrument.nodejs.runasuser, null)#var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
+  autoinstrumentation_nodejs_image                = try(var.appd.autoinstrument.nodejs.image, null)#var.autoinstrument.nodejs.image != null ? var.autoinstrument.nodejs.image : null  # Note: No Latest Image Tag
+  autoinstrumentation_nodejs_imagepullpolicy      = try(var.appd.autoinstrument.nodejs.runasuser,null)#var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
 
   ### Image Information ###
-  imageinfo_clusteragent_image    = try(var.imageinfo.clusteragent.image, null)#var.imageinfo.clusteragent.image != null ? var.imageinfo.clusteragent.image : null
-  imageinfo_clusteragent_tag      = try(var.imageinfo.clusteragent.tag, null)#var.imageinfo.clusteragent.tag != null ? var.imageinfo.clusteragent.tag : null
-  imageinfo_operator_image        = try(var.imageinfo.operator.image, null)#var.imageinfo.operator.image != null ? var.imageinfo.operator.image : null
-  imageinfo_operator_tag          = try(var.imageinfo.operator.tag, null)#var.imageinfo.operator.tag != null ? var.imageinfo.operator.tag : null
-  imageinfo_imagepullpolicy       = try(var.imageinfo.imagepullpolicy, null)#var.imageinfo.imagepullpolicy != null ? var.imageinfo.imagepullpolicy : null
-  imageinfo_machineagent_image    = try(var.imageinfo.machineagent.image, null)#var.imageinfo.machineagent.image != null ? var.imageinfo.machineagent.image : null
-  imageinfo_machineagent_tag      = try(var.imageinfo.machineagent.tag, null)#var.imageinfo.machineagent.tag != null ?  var.imageinfo.machineagent.tag : null
-  imageinfo_machineagentwin_image = try(var.imageinfo.machineagentwin.image, null)#var.imageinfo.machineagentwin.image != null ? var.imageinfo.machineagentwin.image : null
-  imageinfo_machineagentwin_tag   = try(var.imageinfo.machineagentwin.tag, null)#var.imageinfo.machineagentwin.tag != null ? var.imageinfo.machineagentwin.tag : null
-  imageinfo_netviz_image          = try(var.imageinfo.netviz.image, null)#var.imageinfo.netviz.image != null ? var.imageinfo.netviz.image : null
-  imageinfo_netviz_tag            = try(var.imageinfo.netviz.tag, null)#var.imageinfo.netviz.tag != null ? var.imageinfo.netviz.tag : null
+  imageinfo_clusteragent_image    = try(var.appd.imageinfo.clusteragent.image, null)#var.imageinfo.clusteragent.image != null ? var.imageinfo.clusteragent.image : null
+  imageinfo_clusteragent_tag      = try(var.appd.imageinfo.clusteragent.tag, null)#var.imageinfo.clusteragent.tag != null ? var.imageinfo.clusteragent.tag : null
+  imageinfo_operator_image        = try(var.appd.imageinfo.operator.image, null)#var.imageinfo.operator.image != null ? var.imageinfo.operator.image : null
+  imageinfo_operator_tag          = try(var.appd.imageinfo.operator.tag, null)#var.imageinfo.operator.tag != null ? var.imageinfo.operator.tag : null
+  imageinfo_imagepullpolicy       = try(var.appd.imageinfo.imagepullpolicy, null)#var.imageinfo.imagepullpolicy != null ? var.imageinfo.imagepullpolicy : null
+  imageinfo_machineagent_image    = try(var.appd.imageinfo.machineagent.image, null)#var.imageinfo.machineagent.image != null ? var.imageinfo.machineagent.image : null
+  imageinfo_machineagent_tag      = try(var.appd.imageinfo.machineagent.tag, null)#var.imageinfo.machineagent.tag != null ?  var.imageinfo.machineagent.tag : null
+  imageinfo_machineagentwin_image = try(var.appd.imageinfo.machineagentwin.image, null)#var.imageinfo.machineagentwin.image != null ? var.imageinfo.machineagentwin.image : null
+  imageinfo_machineagentwin_tag   = try(var.appd.imageinfo.machineagentwin.tag, null)#var.imageinfo.machineagentwin.tag != null ? var.imageinfo.machineagentwin.tag : null
+  imageinfo_netviz_image          = try(var.appd.imageinfo.netviz.image, null)#var.imageinfo.netviz.image != null ? var.imageinfo.netviz.image : null
+  imageinfo_netviz_tag            = try(var.appd.imageinfo.netviz.tag, null)#var.imageinfo.netviz.tag != null ? var.imageinfo.netviz.tag : null
 }
