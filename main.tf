8cb1dbb483a82ad19b91ee8d40ef5c17d3fd39b0
Richard <richwats@cisco.com>
2022-05-20 10:52:42 +1000

    test

--- main.tf
+++ main.tf
@@ -69,48 +69,48 @@ module "appd" {
   global_account_name = var.appd.account.global_account
 
   ### General Settings ###
-  install_cluster_agent   = try(var.appd.install_metrics_server, null)
-  install_machine_agents  = try(var.appd.install_machine_agents, null)
-  install_metrics_server  = try(var.appd.install_metrics_server, null)
+  install_cluster_agent   = var.appd.install_metrics_server
+  install_machine_agents  = var.appd.install_machine_agents
+  install_metrics_server  = var.appd.install_metrics_server
 
   ### InfraViz ###
-  infraviz_enable_container_hostid  = try(var.appd.infraviz.enable_container_hostid, null)
-  infraviz_enable_dockerviz         = try(var.appd.infraviz.enable_dockerviz, null)
-  infraviz_enable_serverviz         = try(var.appd.infraviz.enable_serverviz, null)
-  infraviz_enable_masters           = try(var.appd.infraviz.enable_masters, null)
-  infraviz_stdout_logging           = try(var.appd.infraviz.stdout_logging, null) # test
+  infraviz_enable_container_hostid  = var.appd.infraviz.enable_container_hostid
+  infraviz_enable_dockerviz         = var.appd.infraviz.enable_dockerviz
+  infraviz_enable_serverviz         = var.appd.infraviz.enable_serverviz
+  infraviz_enable_masters           = var.appd.infraviz.enable_masters
+  infraviz_stdout_logging           = var.appd.infraviz.stdout_logging # test
 
   ### NetViz ###
-  netviz_enabled = try(var.appd.netviz.enabled, null) #var.netiz != null ? var.netviz.enabled : null
+  netviz_enabled = var.appd.netviz.enabled #var.netiz != null ? var.netviz.enabled : null
 
   ### Cluster Agent ###
-  clusteragent_montior_namespace_regex = try(var.appd.cluster.montior_namespace_regex, null) #var.cluster != null ? var.cluster.montior_namespace_regex : null
+  clusteragent_montior_namespace_regex = var.appd.cluster.montior_namespace_regex #var.cluster != null ? var.cluster.montior_namespace_regex : null
 
   ### Auto Instrumentation ###
-  autoinstrumentation_enabled                     = try(var.appd.autoinstrument.enabled, false)# var.autoinstrument != null ? var.autoinstrument.enabled : false
-  autoinstrumentation_namespace_regex             = try(var.appd.autoinstrument.namespace_regex, null)# var.autoinstrument.namespace_regex != null ? var.autoinstrument.namespace_regex : null
-  autoinstrumentation_default_appname             = try(var.appd.autoinstrument.default_appname, null)#var.autoinstrument.default_appname != null ? var.autoinstrument.default_appname : null
-  autoinstrumentation_appname_strategy            = try(var.appd.autoinstrument.appname_strategy, null)#var.autoinstrument.appname_strategy != null ? var.autoinstrument.appname_strategy : null
-  autoinstrumentation_java_runasuser              = try(var.appd.autoinstrument.java.runasuser, null)#var.autoinstrument.java.runasuser != null ? var.autoinstrument.java.runasuser : null
-  autoinstrumentation_java_image                  = try(var.appd.autoinstrument.java.image, null)#var.autoinstrument.java.image != null ? var.autoinstrument.java.image : null
-  autoinstrumentation_java_imagepullpolicy        = try(var.appd.autoinstrument.java.imagepullpolicy, null)#var.autoinstrument.java.imagepullpolicy != null ? var.autoinstrument.java.imagepullpolicy : null
-  autoinstrumentation_dotnetcore_runasuser        = try(var.appd.autoinstrument.dotnetcore.runasuser, null)#var.autoinstrument.dotnetcore.runasuser != null ? var.autoinstrument.dotnetcore.runasuser : null
-  autoinstrumentation_dotnetcore_image            = try(var.appd.autoinstrument.dotnetcore.image, null)#var.autoinstrument.dotnetcore.image != null ? var.autoinstrument.dotnetcore.image : null
-  autoinstrumentation_dotnetcore_imagepullpolicy  = try(var.appd.autoinstrument.dotnetcore.imagepullpolicy, null)#var.autoinstrument.dotnetcore.imagepullpolicy != null ? var.autoinstrument.dotnetcore.imagepullpolicy : null
-  autoinstrumentation_nodejs_runasuser            = try(var.appd.autoinstrument.nodejs.runasuser, null)#var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
-  autoinstrumentation_nodejs_image                = try(var.appd.autoinstrument.nodejs.image, null)#var.autoinstrument.nodejs.image != null ? var.autoinstrument.nodejs.image : null  # Note: No Latest Image Tag
-  autoinstrumentation_nodejs_imagepullpolicy      = try(var.appd.autoinstrument.nodejs.runasuser,null)#var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
+  autoinstrumentation_enabled                     = var.appd.autoinstrument.enabled # var.autoinstrument != null ? var.autoinstrument.enabled : false
+  autoinstrumentation_namespace_regex             = var.appd.autoinstrument.namespace_regex # var.autoinstrument.namespace_regex != null ? var.autoinstrument.namespace_regex : null
+  autoinstrumentation_default_appname             = var.appd.autoinstrument.default_appname #var.autoinstrument.default_appname != null ? var.autoinstrument.default_appname : null
+  autoinstrumentation_appname_strategy            = var.appd.autoinstrument.appname_strategy #var.autoinstrument.appname_strategy != null ? var.autoinstrument.appname_strategy : null
+  autoinstrumentation_java_runasuser              = var.appd.autoinstrument.java.runasuser #var.autoinstrument.java.runasuser != null ? var.autoinstrument.java.runasuser : null
+  autoinstrumentation_java_image                  = var.appd.autoinstrument.java.image #var.autoinstrument.java.image != null ? var.autoinstrument.java.image : null
+  autoinstrumentation_java_imagepullpolicy        = var.appd.autoinstrument.java.imagepullpolicy #var.autoinstrument.java.imagepullpolicy != null ? var.autoinstrument.java.imagepullpolicy : null
+  autoinstrumentation_dotnetcore_runasuser        = var.appd.autoinstrument.dotnetcore.runasuser #var.autoinstrument.dotnetcore.runasuser != null ? var.autoinstrument.dotnetcore.runasuser : null
+  autoinstrumentation_dotnetcore_image            = var.appd.autoinstrument.dotnetcore.image #var.autoinstrument.dotnetcore.image != null ? var.autoinstrument.dotnetcore.image : null
+  autoinstrumentation_dotnetcore_imagepullpolicy  = var.appd.autoinstrument.dotnetcore.imagepullpolicy #var.autoinstrument.dotnetcore.imagepullpolicy != null ? var.autoinstrument.dotnetcore.imagepullpolicy : null
+  autoinstrumentation_nodejs_runasuser            = var.appd.autoinstrument.nodejs.runasuser #var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
+  autoinstrumentation_nodejs_image                = var.appd.autoinstrument.nodejs.image #var.autoinstrument.nodejs.image != null ? var.autoinstrument.nodejs.image : null  # Note: No Latest Image Tag
+  autoinstrumentation_nodejs_imagepullpolicy      = var.appd.autoinstrument.nodejs.runasuser #var.autoinstrument.nodejs.runasuser != null ? var.autoinstrument.nodejs.runasuser : null
 
   ### Image Information ###
-  imageinfo_clusteragent_image    = try(var.appd.imageinfo.clusteragent.image, null)#var.imageinfo.clusteragent.image != null ? var.imageinfo.clusteragent.image : null
-  imageinfo_clusteragent_tag      = try(var.appd.imageinfo.clusteragent.tag, null)#var.imageinfo.clusteragent.tag != null ? var.imageinfo.clusteragent.tag : null
-  imageinfo_operator_image        = try(var.appd.imageinfo.operator.image, null)#var.imageinfo.operator.image != null ? var.imageinfo.operator.image : null
-  imageinfo_operator_tag          = try(var.appd.imageinfo.operator.tag, null)#var.imageinfo.operator.tag != null ? var.imageinfo.operator.tag : null
-  imageinfo_imagepullpolicy       = try(var.appd.imageinfo.imagepullpolicy, null)#var.imageinfo.imagepullpolicy != null ? var.imageinfo.imagepullpolicy : null
-  imageinfo_machineagent_image    = try(var.appd.imageinfo.machineagent.image, null)#var.imageinfo.machineagent.image != null ? var.imageinfo.machineagent.image : null
-  imageinfo_machineagent_tag      = try(var.appd.imageinfo.machineagent.tag, null)#var.imageinfo.machineagent.tag != null ?  var.imageinfo.machineagent.tag : null
-  imageinfo_machineagentwin_image = try(var.appd.imageinfo.machineagentwin.image, null)#var.imageinfo.machineagentwin.image != null ? var.imageinfo.machineagentwin.image : null
-  imageinfo_machineagentwin_tag   = try(var.appd.imageinfo.machineagentwin.tag, null)#var.imageinfo.machineagentwin.tag != null ? var.imageinfo.machineagentwin.tag : null
-  imageinfo_netviz_image          = try(var.appd.imageinfo.netviz.image, null)#var.imageinfo.netviz.image != null ? var.imageinfo.netviz.image : null
-  imageinfo_netviz_tag            = try(var.appd.imageinfo.netviz.tag, null)#var.imageinfo.netviz.tag != null ? var.imageinfo.netviz.tag : null
+  imageinfo_clusteragent_image    = var.appd.imageinfo.clusteragent.image #var.imageinfo.clusteragent.image != null ? var.imageinfo.clusteragent.image : null
+  imageinfo_clusteragent_tag      = var.appd.imageinfo.clusteragent.tag #var.imageinfo.clusteragent.tag != null ? var.imageinfo.clusteragent.tag : null
+  imageinfo_operator_image        = var.appd.imageinfo.operator.image #var.imageinfo.operator.image != null ? var.imageinfo.operator.image : null
+  imageinfo_operator_tag          = var.appd.imageinfo.operator.tag #var.imageinfo.operator.tag != null ? var.imageinfo.operator.tag : null
+  imageinfo_imagepullpolicy       = var.appd.imageinfo.imagepullpolicy #var.imageinfo.imagepullpolicy != null ? var.imageinfo.imagepullpolicy : null
+  imageinfo_machineagent_image    = var.appd.imageinfo.machineagent.image #var.imageinfo.machineagent.image != null ? var.imageinfo.machineagent.image : null
+  imageinfo_machineagent_tag      = var.appd.imageinfo.machineagent.tag #var.imageinfo.machineagent.tag != null ?  var.imageinfo.machineagent.tag : null
+  imageinfo_machineagentwin_image = var.appd.imageinfo.machineagentwin.image #var.imageinfo.machineagentwin.image != null ? var.imageinfo.machineagentwin.image : null
+  imageinfo_machineagentwin_tag   = var.appd.imageinfo.machineagentwin.tag #var.imageinfo.machineagentwin.tag != null ? var.imageinfo.machineagentwin.tag : null
+  imageinfo_netviz_image          = var.appd.imageinfo.netviz.image #var.imageinfo.netviz.image != null ? var.imageinfo.netviz.image : null
+  imageinfo_netviz_tag            = var.appd.imageinfo.netviz.tag #var.imageinfo.netviz.tag != null ? var.imageinfo.netviz.tag : null
 }
--- modules/appd/variables.tf
+++ modules/appd/variables.tf
@@ -8,11 +8,13 @@ variable "namespace" {
 variable "repository" {
   type = string
   default = "https://ciscodevnet.github.io/appdynamics-charts"
+  nullable = false
 }
 
 variable "chart_name" {
   type = string
   default = "cluster-agent"
+  nullable = false
 }
 
 ## AppD Variables ##
@@ -45,6 +47,7 @@ variable "account_password" {
 variable "global_account_name" {
   type = string
   default = ""
+  nullable = false
 }
 
 ### General Settings ###
@@ -52,176 +55,210 @@ variable "global_account_name" {
 variable "install_metrics_server" {
   type = bool
   default = false
+  nullable = false
 }
 
 variable "install_cluster_agent" {
   type = bool
   default = true
+  nullable = false
 }
 
 variable "install_machine_agents" {
   type = bool
   default = false
+  nullable = false
 }
 
 ### InfraViz ###
 variable "infraviz_enable_container_hostid" {
   type = bool
   default = true
+  nullable = false
 }
 
 variable "infraviz_enable_dockerviz" {
   type = bool
   default = true
+  nullable = false
 }
 
 variable "infraviz_enable_serverviz" {
   type = bool
   default = true
+  nullable = false
 }
 
 variable "infraviz_enable_masters" {
   type = bool
   default = false
+  nullable = false
 }
 
 variable "infraviz_stdout_logging" {
   type = bool
   default = false
+  nullable = false
 }
 
 ### NetViz ###
 variable "netviz_enabled" {
   type = bool
   default = false
+  nullable = false
 }
 
 ### Cluster Agent ###
 variable "clusteragent_montior_namespace_regex" {
   default = ".*"
+  nullable = false
 }
 
 ### Auto Instrumentation ###
 variable "autoinstrumentation_enabled" {
   type = bool
   default = false
+  nullable = false
 }
 
 variable "autoinstrumentation_namespace_regex" {
   type    = string
   default = ""
+  nullable = false
 }
 
 variable "autoinstrumentation_default_appname" {
   type    = string
   default = ""
+  nullable = false
 }
 
 variable "autoinstrumentation_appname_strategy" {
   type    = string
   default = "manual"
+  nullable = false
 }
 
 # Java
 variable "autoinstrumentation_java_runasuser" {
   type    = number
   default = 10001
+  nullable = false
 }
 
 variable "autoinstrumentation_java_image" {
   type    = string
   default = "docker.io/appdynamics/java-agent:latest"
+  nullable = false
 }
 
 variable "autoinstrumentation_java_imagepullpolicy" {
   type    = string
   default = "Always"
+  nullable = false
 }
 
 # Dotnet Core
 variable "autoinstrumentation_dotnetcore_runasuser" {
   type    = number
   default = 10001
+  nullable = false
 }
 
 variable "autoinstrumentation_dotnetcore_image" {
   type    = string
   default = "docker.io/appdynamics/dotnet-core-agent:latest"
+  nullable = false
 }
 
 variable "autoinstrumentation_dotnetcore_imagepullpolicy" {
   type    = string
   default = "Always"
+  nullable = false
 }
 
 # Node.js
 variable "autoinstrumentation_nodejs_runasuser" {
   type    = number
   default = 10001
+  nullable = false
 }
 
 variable "autoinstrumentation_nodejs_image" {
   type    = string
   default = "docker.io/appdynamics/nodejs-agent:21.9.0-16-alpine"  ## No Latest Image Tag
+  nullable = false
 }
 
 variable "autoinstrumentation_nodejs_imagepullpolicy" {
   type    = string
   default = "IfNotPresent"
+  nullable = false
 }
 
 ### Image Information ###
 variable "imageinfo_clusteragent_image" {
   type = string
   default = "docker.io/appdynamics/cluster-agent"
+  nullable = false
 }
 
 variable "imageinfo_clusteragent_tag" {
   type = string
   default = "latest"
+  nullable = false
 }
 
 variable "imageinfo_operator_image" {
   type = string
   default = "docker.io/appdynamics/cluster-agent-operator"
+  nullable = false
 }
 
 variable "imageinfo_operator_tag" {
   type = string
   default = "latest"
+  nullable = false
 }
 
 variable "imageinfo_imagepullpolicy" {
   type    = string
   default = "Always"
+  nullable = false
 }
 
 variable "imageinfo_machineagent_image" {
   type = string
   default = "docker.io/appdynamics/machine-agent"
+  nullable = false
 }
 
 variable "imageinfo_machineagent_tag" {
   type = string
   default = "latest"
+  nullable = false
 }
 
 variable "imageinfo_machineagentwin_image" {
   type = string
   default = "docker.io/appdynamics/machine-agent-analytics"
+  nullable = false
 }
 
 variable "imageinfo_machineagentwin_tag" {
   type = string
   default = "win-latest"
+  nullable = false
 }
 
 variable "imageinfo_netviz_image" {
   type = string
   default = "docker.io/appdynamics/machine-agent-netviz"
+  nullable = false
 }
 
 variable "imageinfo_netviz_tag" {
   type = string
   default = "latest"
+  nullable = false
 }
