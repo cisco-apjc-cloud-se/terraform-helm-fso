1338b20ec8f672b067092a6afd278855b49d1e2f
Richard <richwats@cisco.com>
2022-05-26 10:37:03 +1000

    test

--- modules/thousandeyes/main.tf
+++ modules/thousandeyes/main.tf
@@ -13,44 +13,48 @@ locals {
   combined_agents = distinct(flatten([for test in var.http_tests : test.agents ]))
 }
 
-data "thousandeyes_agent" "agents" {
-  for_each = toset(local.combined_agents)
-  agent_name  = each.key
+# data "thousandeyes_agent" "agents" {
+#   for_each = toset(local.combined_agents)
+#   agent_name  = each.key
+# }
+
+data "thousandeyes_agent" "test" {
+  agent_name = "Sydney, Australia"
 }
 
-resource "thousandeyes_http_server" "http_tests" {
-  for_each = var.http_tests
-
-  test_name         = "HTTP-${each.value.name}"
-  interval          = each.value.interval
-  url               = each.value.url
-  content_regex     = each.value.content_regex
-
-  // NOTE: Bug with API/Provider?  Need to save in GUI to match settings
-  // Initial Setup
-  // network_measurements = 0
-  // mtu_measurements = 0
-  // bandwidth_measurements = 0
-  // bgp_measurements = 0
-  // use_public_bgp = 0
-  // // num_path_traces = 0
-
-  // Updated Settings
-  network_measurements    = each.value.network_measurements # 1
-  mtu_measurements        = each.value.mtu_measurements # 1
-  bandwidth_measurements  = each.value.bandwidth_measurements # 0
-  bgp_measurements        = each.value.bgp_measurements # 1
-  use_public_bgp          = each.value.use_public_bgp # 1
-  num_path_traces         = each.value.num_path_traces # 0
-
-  dynamic "agents" {
-    for_each = toset(each.value.agents)
-    content {
-      agent_id = data.thousandeyes_agent.agents[agents.key].agent_id
-    }
-  }
-
-}
+# resource "thousandeyes_http_server" "http_tests" {
+#   for_each = var.http_tests
+#
+#   test_name         = "HTTP-${each.value.name}"
+#   interval          = each.value.interval
+#   url               = each.value.url
+#   content_regex     = each.value.content_regex
+#
+#   // NOTE: Bug with API/Provider?  Need to save in GUI to match settings
+#   // Initial Setup
+#   // network_measurements = 0
+#   // mtu_measurements = 0
+#   // bandwidth_measurements = 0
+#   // bgp_measurements = 0
+#   // use_public_bgp = 0
+#   // // num_path_traces = 0
+#
+#   // Updated Settings
+#   network_measurements    = each.value.network_measurements # 1
+#   mtu_measurements        = each.value.mtu_measurements # 1
+#   bandwidth_measurements  = each.value.bandwidth_measurements # 0
+#   bgp_measurements        = each.value.bgp_measurements # 1
+#   use_public_bgp          = each.value.use_public_bgp # 1
+#   num_path_traces         = each.value.num_path_traces # 0
+#
+#   dynamic "agents" {
+#     for_each = toset(each.value.agents)
+#     content {
+#       agent_id = data.thousandeyes_agent.agents[agents.key].agent_id
+#     }
+#   }
+#
+# }
 
 // resource "thousandeyes_agent_to_server" "icmp_tests" {
 //   for_each = local.icmp_tests
