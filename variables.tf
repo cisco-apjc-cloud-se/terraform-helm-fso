b8b79f65150ce5217346414c8427446d979e2128
Richard <richwats@cisco.com>
2022-06-22 16:14:18 +1000

    non optional objects

--- variables.tf
+++ variables.tf
@@ -27,22 +27,22 @@ variable "appd" {
           image = optional(string)
           tag   = optional(string)
           })
-        operator        = optional(object({
+        operator        = object({
           image = optional(string)
           tag   = optional(string)
-          }))
-        machineagent    = optional(object({
+          })
+        machineagent    = object({
           image = optional(string)
           tag   = optional(string)
-          }))
-        machineagentwin = optional(object({
+          })
+        machineagentwin = object({
           image = optional(string)
           tag   = optional(string)
-          }))
-        netviz          = optional(object({
+          })
+        netviz          = object({
           image = optional(string)
           tag   = optional(string)
-          }))
+          })
         })
       })
     account = object({
@@ -62,81 +62,46 @@ variable "appd" {
       })
     machine_agent = object({
       install_service = bool
-      infraviz = optional(object({
+      infraviz = object({
         enable_container_hostid = optional(bool)
         enable_dockerviz        = optional(bool)
         enable_serverviz        = optional(bool)
         enable_masters          = optional(bool)
         stdout_logging          = optional(bool)
-        }))
-      netviz = optional(object({
+        })
+      netviz = object({
         enabled = optional(bool)
         port    = optional(number)
-        }))
+        })
       })
     cluster_agent = object({
       install_service           = bool
       app_name                  = optional(string)
-      monitor_namespace_regex  = optional(string)
+      monitor_namespace_regex   = optional(string)
       })
     autoinstrument = object({
       enabled           = bool
       namespace_regex   = optional(string)
       default_appname   = optional(string)
       appname_strategy  = optional(string)
-      java = optional(object({
+      java = object({
         enabled         = optional(bool)
         runasuser       = optional(number)
         image           = optional(string)
         imagepullpolicy = optional(string)
-        }))
-      dotnetcore = optional(object({
+        })
+      dotnetcore = object({
         enabled         = optional(bool)
         runasuser       = optional(number)
         image           = optional(string)
         imagepullpolicy = optional(string)
-        }))
-      nodejs = optional(object({
+        })
+      nodejs = object({
         enabled         = optional(bool)
         runasuser       = optional(number)
         image           = optional(string)
         imagepullpolicy = optional(string)
-        }))
+        })
       })
     })
-
-  default = {
-    enabled = true
-    kubernetes = {
-      namespace = ""
-      imageinfo = {
-        clusteragent = {}
-        operator = {}
-        machineagent = {}
-        machineagentwin = {}
-        netviz = {}
-        }
-    }
-    account = {
-      name = ""
-      key  = ""
-    }
-    metrics_server = {
-      install_service = true
-      }
-    machine_agent = {
-      install_service = false
-      infraviz = {}
-      netviz = {}
-      }
-    cluster_agent = {
-      install_service = true
-      }
-    autoinstrument = {
-      enabled           = false
-      java = {}
-      dotnetcore = {}
-      nodejs = {}
-      }
-    }
 }
